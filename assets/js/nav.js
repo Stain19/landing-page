53c283e60ec0f223d1a3385b758ecf0c6d15ceac
Pedro Braga <prometeuslbo@gmail.com>
2024-05-15 13:46:11 -0300

    Commit inicial

---
+++ assets/js/nav.js
@@ -0,0 +1,8 @@
+function showMenuNav(){
+    var elemento = document.getElementById("menu-nav-bar");
+    if (elemento.style.display === "none") {
+        elemento.style.display = "block";
+    } else {
+        elemento.style.display = "none";
+    }
+}
