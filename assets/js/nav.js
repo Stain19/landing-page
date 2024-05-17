bbc497ba07a51bd312980d352e8d3614b69abe73
Pedro Braga <prometeuslbo@gmail.com>
2024-05-17 10:08:12 -0300

    Separando arquivos CSS e adicionando dinamicidade a pagina

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,8 +1,21 @@
 function showMenuNav(){
-    var elemento = document.getElementById("menu-nav-bar");
+    var elemento = document.getElementById("nav-bar");
     if (elemento.style.display === "none") {
         elemento.style.display = "block";
     } else {
         elemento.style.display = "none";
+    }
+}
+
+function buttonScrollToSection(sectionId){
+    var targetElement = document.getElementById(sectionId);
+    var navBar = document.querySelector('.menu-content');
+
+    if(targetElement) {
+        var offSetTop = targetElement.offsetTop - navBar.offsetHeight;
+        window.scrollTo({
+            top: offSetTop,
+            behavior: "smooth"
+        });
     }
 }
